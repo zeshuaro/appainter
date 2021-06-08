34bc5d0397474da799088e876dd08c54b7013c8a
Joshua <joshuaystang@gmail.com>
2021-06-08 21:33:44 +1000

    Fix expandable card background color

--- lib/widgets/cards/card.dart
+++ lib/widgets/cards/card.dart
@@ -3,15 +3,20 @@ import 'package:flutter_theme/common/consts.dart';
 
 class MyCard extends StatelessWidget {
   final Widget child;
+  final EdgeInsets padding;
 
-  const MyCard({Key? key, required this.child}) : super(key: key);
+  const MyCard({
+    Key? key,
+    required this.child,
+    this.padding = kPaddingAll,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       color: Colors.grey[50],
       child: Padding(
-        padding: kPaddingAll,
+        padding: padding,
         child: child,
       ),
     );

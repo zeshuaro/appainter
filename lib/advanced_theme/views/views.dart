189fb969a625dd426f05317e1deb8235771d4c99
Joshua <joshuaystang@gmail.com>
2021-06-29 21:07:07 +1000

    test: add more tests for bottom nav bar editor

--- lib/advanced_theme/views/views.dart
+++ lib/advanced_theme/views/views.dart
@@ -2,3 +2,4 @@ export 'advanced_editor.dart';
 export 'color_editor.dart';
 export 'app_bar/app_bar_editor.dart';
 export 'tab_bar/tab_bar_editor.dart';
+export 'bottom_nav_bar/bottom_nav_bar_editor.dart';

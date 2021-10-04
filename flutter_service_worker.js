6cba47abd0858377136a86804606db14de7c9427
zeshuaro <joshuaystang@gmail.com>
2021-10-04 07:21:16 +0000

    Deploying to gh-pages from @ zeshuaro/flutter_theme@c65ea7291e695176fbc5722026801eff9d2414ce 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "dad20b1e0c9cd67514d2d7132cfd5dc5",
-"/": "dad20b1e0c9cd67514d2d7132cfd5dc5",
+"index.html": "a778723c21d55ee22b587d2efc980833",
+"/": "a778723c21d55ee22b587d2efc980833",
 "assets/NOTICES": "a4e4b1fe208fb0c04bdd58f1079b569c",
 "assets/packages/flex_color_picker/assets/opacity.png": "49c4f3bcb1b25364bb4c255edcaaf5b2",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "eaed33dc9678381a55cb5c13edaf241d",
@@ -16,7 +16,7 @@ const RESOURCES = {
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/AssetManifest.json": "4e597e2960a8863f883676c924ef86f5",
 "version.json": "7f865f12f84b7d47c4e0c7067bae4e3d",
-"manifest.json": "3e95ea7cef5f4611214e19000405ecdf",
+"manifest.json": "6babd0b8352b23e237350e91113d11b4",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "main.dart.js": "e7db7c5c96f2fb832bf36b32515ad4da"

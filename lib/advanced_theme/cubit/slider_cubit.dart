1278212715e237f0f91ff175927f9186fd5e38aa
Joshua <joshuaystang@gmail.com>
2021-10-18 23:09:10 +1100

    feat: add slider theme cubit

---
+++ lib/advanced_theme/cubit/slider_cubit.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+import 'advanced_theme_cubit.dart';
+
+extension SliderCubit on AdvancedThemeCubit {
+  void sliderTrackHeightChanged(String value) {
+    final height = double.tryParse(value);
+    if (height != null) {
+      final theme = state.themeData.sliderTheme.copyWith(trackHeight: height);
+      _emitWithSliderTheme(theme);
+    }
+  }
+
+  void sliderActiveTrackColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(activeTrackColor: color);
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderInactiveTrackColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      inactiveTrackColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderDisabledActiveTrackColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      disabledActiveTrackColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderDisabledInactiveTrackColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      disabledInactiveTrackColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderActiveTickMarkColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      activeTickMarkColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderInactiveTickMarkColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      inactiveTickMarkColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderDisabledActiveTickMarkColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      disabledActiveTickMarkColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderDisabledInactiveTickMarkColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      disabledInactiveTickMarkColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderThumbColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(thumbColor: color);
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderDisabledThumbColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      disabledThumbColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderOverlappingShapeStrokeColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      overlappingShapeStrokeColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderOverlayColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(overlayColor: color);
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderValueIndicatorColorChanged(Color color) {
+    final theme = state.themeData.sliderTheme.copyWith(
+      valueIndicatorColor: color,
+    );
+    _emitWithSliderTheme(theme);
+  }
+
+  void sliderMinThumbSeparationChanged(String value) {
+    final separation = double.tryParse(value);
+    if (separation != null) {
+      final theme = state.themeData.sliderTheme.copyWith(
+        minThumbSeparation: separation,
+      );
+      _emitWithSliderTheme(theme);
+    }
+  }
+
+  void _emitWithSliderTheme(SliderThemeData theme) {
+    emit(
+      state.copyWith(themeData: state.themeData.copyWith(sliderTheme: theme)),
+    );
+  }
+}
---
+++ test/advanced_theme/cubit/slider_cubit_test.dart
@@ -0,0 +1,222 @@
+import 'dart:math';
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_theme/advanced_theme/advanced_theme.dart';
+
+import '../../utils.dart';
+
+void main() {
+  late AdvancedThemeCubit cubit;
+
+  setUp(() {
+    cubit = AdvancedThemeCubit();
+  });
+
+  group('sliderTrackHeightChanged', () {
+    final height = Random().nextDouble();
+    final theme = ThemeData(sliderTheme: SliderThemeData(trackHeight: height));
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider track height changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderTrackHeightChanged(height.toString()),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderActiveTrackColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(activeTrackColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider active track color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderActiveTrackColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderInactiveTrackColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(inactiveTrackColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider inactive track color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderInactiveTrackColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderDisabledActiveTrackColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(disabledActiveTrackColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider disabled active track color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderDisabledActiveTrackColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderDisabledInactiveTrackColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(disabledInactiveTrackColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider disabled inactive track color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderDisabledInactiveTrackColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderActiveTickMarkColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(activeTickMarkColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider active tick mark color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderActiveTickMarkColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderInactiveTickMarkColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(inactiveTickMarkColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider inactive tick mark color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderInactiveTickMarkColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderDisabledActiveTickMarkColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(disabledActiveTickMarkColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider inactive tick mark color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderDisabledActiveTickMarkColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderDisabledInactiveTickMarkColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(disabledInactiveTickMarkColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider disabled inactive tick mark color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderDisabledInactiveTickMarkColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderThumbColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(sliderTheme: SliderThemeData(thumbColor: color));
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider thumb color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderThumbColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderDisabledThumbColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(disabledThumbColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider overlapping shape stroke color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderDisabledThumbColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderOverlappingShapeStrokeColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(overlappingShapeStrokeColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider overlapping shape stroke color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderOverlappingShapeStrokeColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderOverlayColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(sliderTheme: SliderThemeData(overlayColor: color));
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider overlay color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderOverlayColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderValueIndicatorColorChanged', () {
+    final color = getRandomColor();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(valueIndicatorColor: color),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider value indicator color changed',
+      build: () => cubit,
+      act: (cubit) => cubit.sliderValueIndicatorColorChanged(color),
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+
+  group('sliderMinThumbSeparationChanged', () {
+    final separation = Random().nextDouble();
+    final theme = ThemeData(
+      sliderTheme: SliderThemeData(minThumbSeparation: separation),
+    );
+
+    blocTest<AdvancedThemeCubit, AdvancedThemeState>(
+      'should emit slider min thumb separation changed',
+      build: () => cubit,
+      act: (cubit) {
+        cubit.sliderMinThumbSeparationChanged(separation.toString());
+      },
+      expect: () => [AdvancedThemeState(themeData: theme)],
+    );
+  });
+}

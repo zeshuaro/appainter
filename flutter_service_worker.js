e58ce607e9380a74db5f452eb349b31a359cebfb
zeshuaro <joshuaystang@gmail.com>
2021-10-29 23:48:09 +0000

    Deploying to gh-pages from @ zeshuaro/flutter_theme@39c30c7715c596c0bfb4686a02837d186dd886d0 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "dffe293b7a9184904f413a5e76fd91df",
+  "version.json": "b4d6719bab495c03298a5fe18331b267",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "e9173522051b1b144c817b2ee1cf44ad",
@@ -16,10 +16,10 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "738229b536df42e147dcec6899941811",
+"main.dart.js": "eda404ea04bf43ba08347dbee6518ad5",
 "manifest.json": "6babd0b8352b23e237350e91113d11b4",
-"index.html": "958e3ed335c2c77bfdfeb8ffc2ff2230",
-"/": "958e3ed335c2c77bfdfeb8ffc2ff2230"
+"index.html": "a9271484f0ea00c1aaa73d67aeeebee8",
+"/": "a9271484f0ea00c1aaa73d67aeeebee8"
 };
 
 // The application shell files that are downloaded before a service worker can

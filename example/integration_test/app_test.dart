6ba0cc24e4709a3d9c14fe68bc34491f6a959467
zeshuaro <joshuaystang@gmail.com>
2021-12-04 12:34:13 +1100

    test: add e2e test for example app (#159)

---
+++ example/integration_test/app_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_theme_example/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets(
+      'tap on the floating action button, verify counter',
+      (tester) async {
+        app.main();
+        await tester.pumpAndSettle();
+
+        // Verify the counter starts at 0.
+        expect(find.text('0'), findsOneWidget);
+
+        // Finds the floating action button to tap on.
+        final Finder fab = find.byTooltip('Increment');
+
+        // Emulate a tap on the floating action button.
+        await tester.tap(fab);
+
+        // Trigger a frame.
+        await tester.pumpAndSettle();
+
+        // Verify the counter increments by 1.
+        expect(find.text('1'), findsOneWidget);
+      },
+    );
+  });
+}

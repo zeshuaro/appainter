d0eaeef597ee22df01a2bbf8d391dc674cb62c49
Joshua Tang <joshuaystang@gmail.com>
2021-12-19 21:43:14 +1100

    style: disable linting for cubit extensions

--- lib/advanced_theme/cubit/input_decoration_cubit.dart
+++ lib/advanced_theme/cubit/input_decoration_cubit.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
+
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
 

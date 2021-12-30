da07ecef309ee045e00c3bf7df8d039324d652e5
zeshuaro <joshuaystang@gmail.com>
2021-12-30 03:47:33 +0000

    Deploying to gh-pages from @ zeshuaro/flutter_theme@d42ccefe82b03446a1322af30f41333aad43ca4a 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
 "main.dart.js": "3ac2389505ccd8428d45d627e5a33145",
-"version.json": "d476251642a3ebe44fa2c01a0d1d4c74",
+"version.json": "d21cebd251fb44c91f62f9aa7aaa40b6",
 "manifest.json": "6babd0b8352b23e237350e91113d11b4",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -21,8 +21,8 @@ const RESOURCES = {
 "assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "aa1ec80f1b30a51d64c72f669c1326a7",
 "assets/packages/flex_color_picker/assets/opacity.png": "49c4f3bcb1b25364bb4c255edcaaf5b2",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"index.html": "c1a0802387a8e01986fa94efc6ab97c4",
-"/": "c1a0802387a8e01986fa94efc6ab97c4",
+"index.html": "9c5b85b350dd436fa7a7a6e78bbc598c",
+"/": "9c5b85b350dd436fa7a7a6e78bbc598c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796"
 };
 

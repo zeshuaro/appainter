3347ab53e6b931325bf110864bfbfa4754ea967f
Joshua Tang <joshuaystang@gmail.com>
2022-02-23 21:12:51 +1100

    feat: set minimum window size for desktop apps

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,21 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:appainter/home/home.dart';
+import 'package:universal_io/io.dart';
+import 'package:window_size/window_size.dart';
 
 import 'app.dart';
 import 'bloc_observer.dart';
 
 void main() {
   EquatableConfig.stringify = kDebugMode;
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle("Appainter");
+    setWindowMinSize(const Size(1350, 850));
+    setWindowMaxSize(Size.infinite);
+  }
 
   BlocOverrides.runZoned(
     () => runApp(MyApp(

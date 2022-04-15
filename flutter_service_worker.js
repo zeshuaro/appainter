13e3889c5f466a42a1138167d1ee05a60ddb0460
zeshuaro <joshuaystang@gmail.com>
2022-04-15 06:30:29 +0000

    Deploying to gh-pages from @ zeshuaro/appainter@cebc2d6c0f72a4c001264e1797a4b01182e0eedf 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "9e3899820bf01fac7f4845397ba1fcf8",
-"index.html": "adf1d7424490e79bf5b286948d81041e",
-"/": "adf1d7424490e79bf5b286948d81041e",
+"index.html": "58ef9a8575bb51cabcbd1582d3a77553",
+"/": "58ef9a8575bb51cabcbd1582d3a77553",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "ff1eee5d80fd025929453faeff3ab75e",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "b62641afc9ab487008e996a5c5865e56",

c261277ced153ab1cac92d520e11fc6c7e8418fb
Joshua Tang <joshuaystang@gmail.com>
2022-04-15 16:07:49 +1000

    feat(builder): update to fetch property source description

--- packages/builder/lib/src/theme_docs_generator.dart
+++ packages/builder/lib/src/theme_docs_generator.dart
@@ -1,8 +1,9 @@
-import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/element.dart' as analyzer;
 import 'package:appainter_annotations/annotations.dart';
 import 'package:appainter_builder/src/http_client.dart';
 import 'package:build/build.dart';
 import 'package:collection/collection.dart';
+import 'package:html/dom.dart' as html_dom;
 import 'package:html/parser.dart' show parse;
 import 'package:source_gen/source_gen.dart';
 
@@ -11,15 +12,16 @@ class ThemeDocsGenerator extends GeneratorForAnnotation<ThemeDocs> {
 
   const ThemeDocsGenerator({this.client = const HttpClient()}) : super();
 
+  static const _baseUrl = 'https://api.flutter.dev/flutter';
   static const _allowedPropertyTypes = {'color', 'double'};
 
   @override
   Future<String> generateForAnnotatedElement(
-    Element element,
+    analyzer.Element element,
     ConstantReader annotation,
     BuildStep buildStep,
   ) async {
-    if (element is! ClassElement) {
+    if (element is! analyzer.ClassElement) {
       throw InvalidGenerationSourceError(
         'Only classes can be annotated with "ThemeDocumentation". "$element" is'
         'not a ClassElement.',
@@ -30,14 +32,17 @@ class ThemeDocsGenerator extends GeneratorForAnnotation<ThemeDocs> {
     final className = element.name.replaceFirst(RegExp(r'Cubit$'), '');
     late final Map<String, String> props;
     if (className == 'ColorTheme') {
-      props = await _getThemeProperties('ThemeData');
+      props = await _getThemeProperties(
+        'ThemeData',
+        getSourceDescription: false,
+      );
     } else {
       props = await _getThemeProperties(className);
     }
 
     final buffer = StringBuffer('class ${className}Docs {');
     for (var prop in props.entries) {
-      buffer.writeln("static const ${prop.key} = '${prop.value}';");
+      buffer.writeln('static const ${prop.key} = "${prop.value}";');
     }
     buffer.writeln('}');
 
@@ -47,10 +52,9 @@ class ThemeDocsGenerator extends GeneratorForAnnotation<ThemeDocs> {
   Future<Map<String, String>> _getThemeProperties(
     String className, {
     Set<String> allowedPropertyTypes = _allowedPropertyTypes,
+    bool getSourceDescription = true,
   }) async {
-    final res = await client.get(
-      'https://api.flutter.dev/flutter/material/$className-class.html',
-    );
+    final res = await client.get('$_baseUrl/material/$className-class.html');
     final document = parse(res.body);
 
     final propsElem = document.querySelector('dl.properties')!;
@@ -73,15 +77,45 @@ class ThemeDocsGenerator extends GeneratorForAnnotation<ThemeDocs> {
       }
 
       final propName = propElem.querySelector('span.name')!.text;
-      propDescElem.querySelector('div')!.remove();
-      final propDesc = propDescElem.text
+      String? propDesc;
+      if (getSourceDescription) {
+        propDesc = await _getPropertyDescription(propName, propDescElem);
+      }
+
+      if (propDesc == null) {
+        propDescElem.querySelector('div')!.remove();
+        propDesc = propDescElem.text;
+      }
+
+      propsMap[propName] = propDesc
           .replaceFirst('[...]', '')
           .trim()
           .replaceAll(RegExp(r'^\s+', multiLine: true), '')
           .replaceAll('\n', ' ');
-      propsMap[propName] = propDesc;
     }
 
     return propsMap;
+  }
+
+  Future<String?> _getPropertyDescription(
+    String name,
+    html_dom.Element element,
+  ) async {
+    final linkElems = element.getElementsByTagName('a');
+    try {
+      final targetLinkElem = linkElems.firstWhere(
+        (element) => element.text.contains(name),
+      );
+      final url = "$_baseUrl/${targetLinkElem.attributes['href']!}";
+      final res = await client.get(url);
+      final document = parse(res.body);
+
+      return document
+          .querySelector('section.desc.markdown')
+          ?.querySelector('p')
+          ?.text;
+    } on StateError {
+      return null;
+    }
   }
 }

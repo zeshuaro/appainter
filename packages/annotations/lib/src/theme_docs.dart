3ff1b5502655e2169e8a480e83a3a8fff6913dfd
Joshua Tang <joshuaystang@gmail.com>
2022-04-25 22:22:32 +1000

    feat(packages): update to allow user defined property types (#384)

--- packages/annotations/lib/src/theme_docs.dart
+++ packages/annotations/lib/src/theme_docs.dart
@@ -1,3 +1,7 @@
 class ThemeDocs {
-  const ThemeDocs();
+  final Set<String> propertyTypes;
+
+  const ThemeDocs({this.propertyTypes = defaultPropertyTyles});
+
+  static const defaultPropertyTyles = {'color', 'double', 'bool'};
 }

a920a87dbc04dcd634dee767d49fb1b94342715e
Joshua Tang <joshuaystang@gmail.com>
2022-05-13 22:46:21 +1000

    refactor: move shared widget to abstract text style editor

--- lib/abstract_text_style/view/abstract_text_style_editor.dart
+++ lib/abstract_text_style/view/abstract_text_style_editor.dart
@@ -1,4 +1,7 @@
 import 'package:appainter/abstract_text_style/abstract_text_style.dart';
+import 'package:appainter/common/common.dart';
+import 'package:appainter/models/models.dart';
+import 'package:appainter/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:appainter/widgets/widgets.dart';
@@ -12,22 +15,114 @@ abstract class AbstractTextStyleEditor<T extends AbstractTextStyleCubit>
     return BlocBuilder<T, TextStyleState>(
       builder: (context, state) {
         final cubit = context.read<T>();
-        return TextStyleCard(
-          style: state.style,
-          onColorChanged: cubit.colorChanged,
-          onBackgroundColorChanged: cubit.backgroundColorChanged,
-          onFontSizeChanged: cubit.fontSizeChanged,
-          onFontWeightChanged: cubit.fontWeightChanged,
-          onFontStyleChanged: cubit.fontStyleChanged,
-          onLetterSpacingChanged: cubit.letterSpacingChanged,
-          onWordSpacingChanged: cubit.wordSpacingChanged,
-          onTextBaselineChanged: cubit.textBaselineChanged,
-          onHeightChanged: cubit.heightChanged,
-          onLeadingDistributionChanged: cubit.leadingDistributionChanged,
-          onDecorationChanged: cubit.decorationChanged,
-          onDecorationColorChanged: cubit.decorationColorChanged,
-          onDecorationStyleChanged: cubit.decorationStyleChanged,
-          onDecorationThicknessChanged: cubit.decorationThicknessChanged,
+        final style = state.style;
+
+        return SideBySideList(
+          padding: kPaddingAll,
+          children: [
+            ColorListTile(
+              key: const Key('colorPicker'),
+              title: 'Color',
+              color: style.color!,
+              onColorChanged: cubit.colorChanged,
+            ),
+            ColorListTile(
+              key: const Key('backgroundColorPicker'),
+              title: 'Background color',
+              color: style.backgroundColor ?? Colors.transparent,
+              onColorChanged: cubit.backgroundColorChanged,
+            ),
+            MyTextFormField(
+              key: const Key('fontSizeTextField'),
+              labelText: 'Font size',
+              onChanged: cubit.fontSizeChanged,
+              initialValue: style.fontSize!.toString(),
+            ),
+            MyTextFormField(
+              key: const Key('heightTextField'),
+              labelText: 'Height',
+              onChanged: cubit.heightChanged,
+              initialValue: style.height?.toString(),
+            ),
+            DropdownListTile(
+              key: const Key('fontWeightDropdown'),
+              title: 'Font weight',
+              value: MyFontWeight().convertToString(style.fontWeight!)!,
+              values: MyFontWeight().names,
+              onChanged: cubit.fontWeightChanged,
+            ),
+            DropdownListTile(
+              key: const Key('fontStyleDropdown'),
+              title: 'Font style',
+              value: UtilService.enumToString(
+                style.fontStyle ?? FontStyle.normal,
+              ),
+              values: UtilService.getEnumStrings(FontStyle.values),
+              onChanged: cubit.fontStyleChanged,
+            ),
+            MyTextFormField(
+              key: const Key('letterSpacingTextField'),
+              labelText: 'Letter spacing',
+              onChanged: cubit.letterSpacingChanged,
+              initialValue: style.letterSpacing?.toString(),
+              allowNegative: true,
+            ),
+            MyTextFormField(
+              key: const Key('wordSpacingTextField'),
+              labelText: 'Word spacing',
+              onChanged: cubit.wordSpacingChanged,
+              initialValue: style.wordSpacing?.toString(),
+              allowNegative: true,
+            ),
+            DropdownListTile(
+              key: const Key('textBaselineDropdown'),
+              title: 'Text baseline',
+              value: UtilService.enumToString(style.textBaseline!),
+              values: UtilService.getEnumStrings(TextBaseline.values),
+              onChanged: cubit.textBaselineChanged,
+            ),
+            DropdownListTile(
+              key: const Key('leadingDistributicubit.dropdown'),
+              title: 'Leading distribution',
+              value: UtilService.enumToString(
+                style.leadingDistribution ??
+                    TextLeadingDistribution.proportional,
+              ),
+              values: UtilService.getEnumStrings(
+                TextLeadingDistribution.values,
+              ),
+              onChanged: cubit.leadingDistributionChanged,
+            ),
+            DropdownListTile(
+              key: const Key('decoraticubit.dropdown'),
+              title: 'Decoration',
+              value: MyTextDecoration().convertToString(style.decoration!)!,
+              values: MyTextDecoration().names,
+              onChanged: cubit.decorationChanged,
+            ),
+            ColorListTile(
+              key: const Key('decorationColorPicker'),
+              title: 'Decoration color',
+              color: style.decorationColor ?? Colors.transparent,
+              onColorChanged: cubit.decorationColorChanged,
+            ),
+            DropdownListTile(
+              key: const Key('decorationStyleDropdown'),
+              title: 'Decoration style',
+              value: UtilService.enumToString(style.decorationStyle),
+              values: UtilService.getEnumStrings(
+                TextDecorationStyle.values,
+                withNull: true,
+              ),
+              onChanged: cubit.decorationStyleChanged,
+            ),
+            MyTextFormField(
+              key: const Key('decorationThicknessTextField'),
+              labelText: 'Decoration thickness',
+              initialValue: style.decorationThickness?.toString(),
+              onChanged: cubit.decorationThicknessChanged,
+            ),
+          ],
         );
       },
     );
--- lib/widgets/cards/cards.dart
+++ lib/widgets/cards/cards.dart
@@ -1,4 +1,3 @@
 export 'card.dart';
 export 'icon_theme_card.dart';
-export 'text_style_card.dart';
 export 'material_states_card.dart';

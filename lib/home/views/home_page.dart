72b15150a6cc74cf82894384d53571cd70b7e029
Joshua Tang <joshuaystang@gmail.com>
2022-05-16 20:09:49 +1000

    chore: run pub get on flutter 3.0.0

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  static const _sdkVersion = '2.10.5';
+  static const _sdkVersion = '3.0.0';
   static final _backgroundColorDark = Colors.grey[900]!;
   static final _backgroundColorLight = Colors.grey[200]!;
 

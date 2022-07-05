b28c798a6216fac396812854413f9aa3f162abe0
zeshuaro <joshuaystang@gmail.com>
2022-07-05 08:18:17 +0000

    Deploying to gh-pages from @ zeshuaro/appainter@6ebd0e495eef3eaf5a21292387ebb544d2529cf0 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,8 +7,8 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"index.html": "6d8bb59909836ff21efe49d3a7f3c4a4",
-"/": "6d8bb59909836ff21efe49d3a7f3c4a4",
+"index.html": "181488eb9f87d637174cd449f212a1ec",
+"/": "181488eb9f87d637174cd449f212a1ec",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/apple-touch-icon.png": "b1e4bbb6ad1d9d7f3ae57d46af16d2a9",
 "icons/android-chrome-512x512.png": "f397aae3a660e3af9dfee292ee3e2bf9",
@@ -25,7 +25,7 @@ const RESOURCES = {
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "b62641afc9ab487008e996a5c5865e56",
 "assets/packages/flex_color_picker/assets/opacity.png": "49c4f3bcb1b25364bb4c255edcaaf5b2",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/NOTICES": "ee1592873393d1c6ddb846450bb421b7",
+"assets/NOTICES": "fd12148fa099c088148d09fcda305c14",
 "main.dart.js": "dce17eaef1038ce0a28f23fc385bc96e"
 };
 

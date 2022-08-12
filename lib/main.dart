d54238bff8a1245ce5471bb303d7841ebb01c7c5
Joshua Tang <joshuaystang@gmail.com>
2022-08-12 18:26:58 +1000

    Fix deprecated bloc api

--- lib/main.dart
+++ lib/main.dart
@@ -14,37 +14,33 @@ import 'package:window_size/window_size.dart';
 import 'app.dart';
 import 'bloc_observer.dart';
 
-void main() {
-  BlocOverrides.runZoned(
-    () async {
-      EquatableConfig.stringify = kDebugMode;
-      WidgetsFlutterBinding.ensureInitialized();
-      late final AnalyticsRepository analyticsRepo;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  EquatableConfig.stringify = kDebugMode;
 
-      if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-        setWindowTitle("Appainter");
-        setWindowMinSize(const Size(1350, 850));
-        setWindowMaxSize(Size.infinite);
-      }
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle("Appainter");
+    setWindowMinSize(const Size(1350, 850));
+    setWindowMaxSize(Size.infinite);
+  }
 
-      if (kIsWeb || Platform.isMacOS) {
-        await Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        );
-        analyticsRepo = AnalyticsRepository();
-        final authRepo = AuthenticationRepository(analyticsRepo: analyticsRepo);
-        await authRepo.user.first;
-        analyticsRepo.logAppOpen();
-      } else {
-        analyticsRepo = EmptyAnalyticsRepository();
-      }
+  late final AnalyticsRepository analyticsRepo;
+  if (kIsWeb || Platform.isMacOS) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    analyticsRepo = AnalyticsRepository();
+    final authRepo = AuthenticationRepository(analyticsRepo: analyticsRepo);
+    await authRepo.user.first;
+    analyticsRepo.logAppOpen();
+  } else {
+    analyticsRepo = EmptyAnalyticsRepository();
+  }
 
-      runApp(MyApp(
-        homeRepo: HomeRepository(),
-        textThemeRepo: TextThemeRepository(),
-        analyticsRepo: analyticsRepo,
-      ));
-    },
-    blocObserver: MyBlocObserver(),
-  );
+  runApp(MyApp(
+    homeRepo: HomeRepository(),
+    textThemeRepo: TextThemeRepository(),
+    analyticsRepo: analyticsRepo,
+  ));
 }

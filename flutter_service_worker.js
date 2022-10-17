e89aa19bc26b35fa9b5e97ae2db89c4af5df9c50
zeshuaro <joshuaystang@gmail.com>
2022-10-17 11:34:33 +0000

    Deploying to gh-pages from @ zeshuaro/appainter@3fcc6e36966b8ea9bf9f915d32a69b3c65304eb4 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "8e8ed0059dc31f861568259c47c68e4c",
 "favicon.ico": "02cc314f1888171f9ec195c1a97d1acb",
-"index.html": "48eabe6ca484a8587f99c378379d4d97",
-"/": "48eabe6ca484a8587f99c378379d4d97",
+"index.html": "b371c99ed286238c58b5fd3d6faacfec",
+"/": "b371c99ed286238c58b5fd3d6faacfec",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "447fc2245bc32d7418ff21db53c64d75",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",

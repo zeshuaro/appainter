d73a06180af53f84bc691e27e5c046b92f07fef9
Joshua Tang <joshuaystang@gmail.com>
2023-02-02 18:20:17 +1100

    test: refactor app bar theme editor tests (#609)

--- lib/app_bar_theme/cubit/app_bar_theme_cubit.dart
+++ lib/app_bar_theme/cubit/app_bar_theme_cubit.dart
@@ -8,6 +8,7 @@ import 'package:bloc/bloc.dart';
 import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 part 'app_bar_theme_cubit.g.dart';
 part 'app_bar_theme_state.dart';
--- lib/app_bar_theme/cubit/app_bar_theme_state.dart
+++ lib/app_bar_theme/cubit/app_bar_theme_state.dart
@@ -6,6 +6,30 @@ class AppBarThemeState extends Equatable {
 
   const AppBarThemeState({this.theme = const AppBarTheme()});
 
+  factory AppBarThemeState.withTheme({
+    Color? backgroundColor,
+    Color? foregroundColor,
+    double? elevation,
+    Color? shadowColor,
+    bool? centerTitle,
+    double? titleSpacing,
+    double? toolbarHeight,
+    SystemUiOverlayStyle? systemUiOverlayStyle,
+  }) {
+    return AppBarThemeState(
+      theme: AppBarTheme(
+        backgroundColor: backgroundColor,
+        foregroundColor: foregroundColor,
+        elevation: elevation,
+        shadowColor: shadowColor,
+        centerTitle: centerTitle,
+        titleSpacing: titleSpacing,
+        toolbarHeight: toolbarHeight,
+        systemOverlayStyle: systemUiOverlayStyle,
+      ),
+    );
+  }
+
   @override
   List<Object> get props => [theme];
 }

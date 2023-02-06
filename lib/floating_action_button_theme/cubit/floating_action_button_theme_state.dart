e2b36e9194b7d7730ea9160cfc8872ba2f65637d
Joshua Tang <joshuaystang@gmail.com>
2023-02-06 21:32:35 +1100

    test: refactor floating action button theme editor tests

--- lib/floating_action_button_theme/cubit/floating_action_button_theme_state.dart
+++ lib/floating_action_button_theme/cubit/floating_action_button_theme_state.dart
@@ -8,6 +8,34 @@ class FloatingActionButtonThemeState extends Equatable {
     this.theme = const FloatingActionButtonThemeData(),
   });
 
+  factory FloatingActionButtonThemeState.withTheme({
+    Color? foregroundColor,
+    Color? backgroundColor,
+    Color? focusColor,
+    Color? hoverColor,
+    Color? splashColor,
+    double? elevation,
+    double? focusElevation,
+    double? hoverElevation,
+    double? disabledElevation,
+    double? highlightElevation,
+  }) {
+    return FloatingActionButtonThemeState(
+      theme: FloatingActionButtonThemeData(
+        backgroundColor: backgroundColor,
+        foregroundColor: foregroundColor,
+        focusColor: focusColor,
+        hoverColor: hoverColor,
+        splashColor: splashColor,
+        elevation: elevation,
+        disabledElevation: disabledElevation,
+        focusElevation: focusElevation,
+        highlightElevation: highlightElevation,
+        hoverElevation: hoverElevation,
+      ),
+    );
+  }
+
   @override
   List<Object> get props => [theme];
 }

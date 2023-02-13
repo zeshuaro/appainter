2ddb50cbe72be6b6890ec733ab9178b90f10f44f
Joshua Tang <joshuaystang@gmail.com>
2023-02-13 23:55:20 +1100

    test: refactor text button theme editor (#628)

--- lib/text_button_theme/cubit/text_button_theme_state.dart
+++ lib/text_button_theme/cubit/text_button_theme_state.dart
@@ -1,10 +1,31 @@
 part of 'text_button_theme_cubit.dart';
 
 @CopyWith()
+@immutable
 class TextButtonThemeState extends Equatable {
   final TextButtonThemeData theme;
 
   const TextButtonThemeState({this.theme = const TextButtonThemeData()});
+
+  factory TextButtonThemeState.withTheme({
+    MaterialStateProperty<Color?>? backgroundColor,
+    MaterialStateProperty<Color?>? foregroundColor,
+    MaterialStateProperty<Color?>? overlayColor,
+    MaterialStateProperty<Color?>? shadowColor,
+    MaterialStateProperty<double?>? elevation,
+  }) {
+    return TextButtonThemeState(
+      theme: TextButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: backgroundColor,
+          foregroundColor: foregroundColor,
+          overlayColor: overlayColor,
+          shadowColor: shadowColor,
+          elevation: elevation,
+        ),
+      ),
+    );
+  }
 
   @override
   List<Object> get props => [theme];

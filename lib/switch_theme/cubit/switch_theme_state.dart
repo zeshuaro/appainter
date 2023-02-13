1ca9d2d4545b474fd0cd8135539e981a8148f530
Joshua Tang <joshuaystang@gmail.com>
2023-02-13 23:16:33 +1100

    test: refactor switch theme editor tests

--- lib/switch_theme/cubit/switch_theme_state.dart
+++ lib/switch_theme/cubit/switch_theme_state.dart
@@ -1,10 +1,29 @@
 part of 'switch_theme_cubit.dart';
 
 @CopyWith()
+@immutable
 class SwitchThemeState extends Equatable {
   final SwitchThemeData theme;
 
   const SwitchThemeState({this.theme = const SwitchThemeData()});
+
+  factory SwitchThemeState.withTheme({
+    MaterialStateProperty<Color?>? thumbColor,
+    MaterialStateProperty<Color?>? trackColor,
+    MaterialStateProperty<Color?>? overlayColor,
+    double? splashRadius,
+    MaterialTapTargetSize? materialTapTargetSize,
+  }) {
+    return SwitchThemeState(
+      theme: SwitchThemeData(
+        thumbColor: thumbColor,
+        trackColor: trackColor,
+        overlayColor: overlayColor,
+        splashRadius: splashRadius,
+        materialTapTargetSize: materialTapTargetSize,
+      ),
+    );
+  }
 
   @override
   List<Object> get props => [theme];

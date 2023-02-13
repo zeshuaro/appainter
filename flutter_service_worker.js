27f4eec4806254b5abfbbde78a175985782984f4
zeshuaro <joshuaystang@gmail.com>
2023-02-13 11:40:15 +0000

    Deploying to gh-pages from @ zeshuaro/appainter@71f05160c16501708c8b7b72749158ebe8362ae8 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
-"index.html": "f35d416d134b6566a2524d91be02c9b9",
-"/": "f35d416d134b6566a2524d91be02c9b9",
-"main.dart.js": "d747529b41f2a0072574416d2b0bc349",
+"index.html": "0064a0e26d81d73def768698171d0242",
+"/": "0064a0e26d81d73def768698171d0242",
+"main.dart.js": "1137568aa41864ef000de90eb8443266",
 "manifest.json": "447fc2245bc32d7418ff21db53c64d75",
 "favicon.ico": "02cc314f1888171f9ec195c1a97d1acb",
 "icons/android-chrome-512x512.png": "f397aae3a660e3af9dfee292ee3e2bf9",
@@ -27,7 +27,7 @@ const RESOURCES = {
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
-"version.json": "980033ba5a3b891fedb297c694c41482"
+"version.json": "82f94227a28192b1bc1ef319fd2ff4c3"
 };
 
 // The application shell files that are downloaded before a service worker can

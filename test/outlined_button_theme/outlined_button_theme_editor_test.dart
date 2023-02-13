9103cad9c96b30de8445b722d30011e54190579b
Joshua Tang <joshuaystang@gmail.com>
2023-02-13 20:48:49 +1100

    test: refactor outlined button theme editor tests

--- test/outlined_button_theme/outlined_button_theme_editor_test.dart
+++ test/outlined_button_theme/outlined_button_theme_editor_test.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:appainter/color_theme/color_theme.dart';
 import 'package:appainter/outlined_button_theme/outlined_button_theme.dart';
 import 'package:appainter/widgets/widgets.dart';
-import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -11,208 +10,238 @@ import 'package:mocktail/mocktail.dart';
 
 import '../mocks.dart';
 import '../utils.dart';
-import '../widget_testers.dart';
+import '../utils/widget_tester_utils.dart';
 
 Future<void> main() async {
-  final widgetTesters = WidgetTesters(expandText: 'Outlined button');
+  const expandText = 'Outlined button';
+  const outlinedButtonThemeState = OutlinedButtonThemeState();
 
   late OutlinedButtonThemeCubit outlinedButtonThemeCubit;
   late ColorThemeCubit colorThemeCubit;
+
   late Color color;
-  late double doubleValue;
+  late double doubleNum;
+  late String doubleStr;
 
   setUp(() {
     outlinedButtonThemeCubit = MockOutlinedButtonThemeCubit();
     colorThemeCubit = MockColorThemeCubit();
-    color = getRandomColor();
-    doubleValue = Random().nextDouble();
 
-    when(() {
-      return outlinedButtonThemeCubit.state;
-    }).thenReturn(const OutlinedButtonThemeState());
+    color = getRandomColor();
+    doubleNum = Random().nextDouble();
+    doubleStr = doubleNum.toString();
+
     when(() => colorThemeCubit.state).thenReturn(ColorThemeState());
   });
 
   Future<void> pumpApp(
-    WidgetTester tester,
-    OutlinedButtonThemeState state,
-  ) async {
-    whenListen(
-      outlinedButtonThemeCubit,
-      Stream.fromIterable([const OutlinedButtonThemeState(), state]),
+    WidgetTester tester, [
+    OutlinedButtonThemeState? state,
+  ]) async {
+    when(() => outlinedButtonThemeCubit.state).thenReturn(
+      state ?? outlinedButtonThemeState,
     );
 
     await tester.pumpWidget(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider.value(value: outlinedButtonThemeCubit),
-          BlocProvider.value(value: colorThemeCubit),
-        ],
-        child: MaterialApp(
-          home: MyExpansionPanelList(item: const OutlinedButtonThemeEditor()),
+      MaterialApp(
+        home: MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: outlinedButtonThemeCubit),
+            BlocProvider.value(value: colorThemeCubit),
+          ],
+          child: Scaffold(
+            body: MyExpansionPanelList(
+              item: const OutlinedButtonThemeEditor(),
+            ),
+          ),
         ),
       ),
     );
+    await tester.expandWidget(expandText);
   }
 
-  testWidgets(
-    'background default color picker should update with selected color',
-    (tester) async {
-      final color = getRandomColor();
+  group('background color picker', () {
+    const key = 'outlinedButtonThemeEditor_backgroundColor_default';
+
+    testWidgets('render widget', (tester) async {
       final prop = getMaterialStateProperty({null: color});
-      final state = OutlinedButtonThemeState(
-        theme: OutlinedButtonThemeData(
-          style: ButtonStyle(backgroundColor: prop),
-        ),
-      );
+      final state = OutlinedButtonThemeState.withTheme(backgroundColor: prop);
 
       await pumpApp(tester, state);
 
-      await widgetTesters.checkColorPicker(
-        tester,
-        'outlinedButtonThemeEditor_backgroundColor_default',
-        color,
-      );
-    },
-  );
+      await tester.expectColorIndicator(key, color);
+    });
 
-  group('test foreground color', () {
-    testWidgets(
-      'default color picker should update with selected color',
-      (tester) async {
+    testWidgets('change color', (tester) async {
+      final opaqueColor = color.withOpacity(0);
+      await pumpApp(tester);
+      await tester.verifyColorPicker(
+        key,
+        opaqueColor,
+        outlinedButtonThemeCubit.backgroundColorChanged,
+      );
+    });
+  });
+
+  group('foreground color pickers', () {
+    group('default', () {
+      const key = 'outlinedButtonThemeEditor_foregroundColor_default';
+
+      testWidgets('render widget', (tester) async {
         final prop = getMaterialStateProperty({null: color});
-        final state = OutlinedButtonThemeState(
-          theme: OutlinedButtonThemeData(
-            style: ButtonStyle(foregroundColor: prop),
-          ),
-        );
+        final state = OutlinedButtonThemeState.withTheme(foregroundColor: prop);
 
         await pumpApp(tester, state);
 
-        await widgetTesters.checkColorPicker(
-          tester,
-          'outlinedButtonThemeEditor_foregroundColor_default',
-          color,
-        );
-      },
-    );
+        await tester.expectColorIndicator(key, color);
+      });
 
-    testWidgets(
-      'disabled color picker should update with selected color',
-      (tester) async {
+      testWidgets('change color', (tester) async {
+        await pumpApp(tester);
+        await tester.verifyColorPicker(
+          key,
+          color,
+          outlinedButtonThemeCubit.foregroundDefaultColorChanged,
+        );
+      });
+    });
+
+    group('disabled', () {
+      const key = 'outlinedButtonThemeEditor_foregroundColor_disabled';
+
+      testWidgets('render widget', (tester) async {
         final prop = getMaterialStateProperty({MaterialState.disabled: color});
-        final state = OutlinedButtonThemeState(
-          theme: OutlinedButtonThemeData(
-            style: ButtonStyle(foregroundColor: prop),
-          ),
-        );
+        final state = OutlinedButtonThemeState.withTheme(foregroundColor: prop);
 
         await pumpApp(tester, state);
 
-        await widgetTesters.checkColorPicker(
-          tester,
-          'outlinedButtonThemeEditor_foregroundColor_disabled',
-          color,
+        await tester.expectColorIndicator(key, color);
+      });
+
+      testWidgets('change color', (tester) async {
+        final opaqueColor = color.withOpacity(0.38);
+        await pumpApp(tester);
+        await tester.verifyColorPicker(
+          key,
+          opaqueColor,
+          outlinedButtonThemeCubit.foregroundDisabledColorChanged,
         );
-      },
-    );
+      });
+    });
   });
 
-  group('test overlay color', () {
-    testWidgets(
-      'hovered color picker should update with selected color',
-      (tester) async {
+  group('overlay color pickers', () {
+    group('hovered', () {
+      const key = 'outlinedButtonThemeEditor_overlayColor_hovered';
+
+      testWidgets('render widget', (tester) async {
         final prop = getMaterialStateProperty({MaterialState.hovered: color});
-        final state = OutlinedButtonThemeState(
-          theme: OutlinedButtonThemeData(
-            style: ButtonStyle(overlayColor: prop),
-          ),
-        );
+        final state = OutlinedButtonThemeState.withTheme(overlayColor: prop);
 
         await pumpApp(tester, state);
 
-        await widgetTesters.checkColorPicker(
-          tester,
-          'outlinedButtonThemeEditor_overlayColor_hovered',
-          color,
-        );
-      },
-    );
+        await tester.expectColorIndicator(key, color);
+      });
 
-    testWidgets(
-      'focused color picker should update with selected color',
-      (tester) async {
+      testWidgets('change color', (tester) async {
+        final opaqueColor = color.withOpacity(0.04);
+        await pumpApp(tester);
+        await tester.verifyColorPicker(
+          key,
+          opaqueColor,
+          outlinedButtonThemeCubit.overlayHoveredColorChanged,
+        );
+      });
+    });
+
+    group('focused', () {
+      const key = 'outlinedButtonThemeEditor_overlayColor_focused';
+
+      testWidgets('render widget', (tester) async {
         final prop = getMaterialStateProperty({MaterialState.focused: color});
-        final state = OutlinedButtonThemeState(
-          theme: OutlinedButtonThemeData(
-            style: ButtonStyle(overlayColor: prop),
-          ),
-        );
+        final state = OutlinedButtonThemeState.withTheme(overlayColor: prop);
 
         await pumpApp(tester, state);
 
-        await widgetTesters.checkColorPicker(
-          tester,
-          'outlinedButtonThemeEditor_overlayColor_focused',
-          color,
-        );
-      },
-    );
+        await tester.expectColorIndicator(key, color);
+      });
 
-    testWidgets(
-      'pressed color picker should update with selected color',
-      (tester) async {
+      testWidgets('change color', (tester) async {
+        final opaqueColor = color.withOpacity(0.12);
+        await pumpApp(tester);
+        await tester.verifyColorPicker(
+          key,
+          opaqueColor,
+          outlinedButtonThemeCubit.overlayFocusedColorChanged,
+        );
+      });
+    });
+
+    group('pressed', () {
+      const key = 'outlinedButtonThemeEditor_overlayColor_pressed';
+
+      testWidgets('render widget', (tester) async {
         final prop = getMaterialStateProperty({MaterialState.pressed: color});
-        final state = OutlinedButtonThemeState(
-          theme: OutlinedButtonThemeData(
-            style: ButtonStyle(overlayColor: prop),
-          ),
-        );
+        final state = OutlinedButtonThemeState.withTheme(overlayColor: prop);
 
         await pumpApp(tester, state);
 
-        await widgetTesters.checkColorPicker(
-          tester,
-          'outlinedButtonThemeEditor_overlayColor_pressed',
-          color,
+        await tester.expectColorIndicator(key, color);
+      });
+
+      testWidgets('change color', (tester) async {
+        final opaqueColor = color.withOpacity(0.12);
+        await pumpApp(tester);
+        await tester.verifyColorPicker(
+          key,
+          opaqueColor,
+          outlinedButtonThemeCubit.overlayPressedColorChanged,
         );
-      },
-    );
+      });
+    });
   });
 
-  testWidgets(
-    'shadow default color picker should update with selected color',
-    (tester) async {
+  group('shadow color picker', () {
+    const key = 'outlinedButtonThemeEditor_shadowColor_default';
+
+    testWidgets('render widget', (tester) async {
       final prop = getMaterialStateProperty({null: color});
-      final state = OutlinedButtonThemeState(
-        theme: OutlinedButtonThemeData(style: ButtonStyle(shadowColor: prop)),
-      );
+      final state = OutlinedButtonThemeState.withTheme(shadowColor: prop);
 
       await pumpApp(tester, state);
 
-      await widgetTesters.checkColorPicker(
-        tester,
-        'outlinedButtonThemeEditor_shadowColor_default',
+      await tester.expectColorIndicator(key, color);
+    });
+
+    testWidgets('change color', (tester) async {
+      await pumpApp(tester);
+      await tester.verifyColorPicker(
+        key,
         color,
+        outlinedButtonThemeCubit.shadowColorChanged,
       );
-    },
-  );
+    });
+  });
 
-  testWidgets(
-    'elevation default text field should update with value',
-    (tester) async {
-      final prop = getMaterialStateProperty({null: doubleValue});
-      final state = OutlinedButtonThemeState(
-        theme: OutlinedButtonThemeData(style: ButtonStyle(elevation: prop)),
-      );
+  group('elevation text field', () {
+    const key = 'outlinedButtonThemeEditor_elevationTextField_default';
+
+    testWidgets('render widget', (tester) async {
+      final prop = getMaterialStateProperty({null: doubleNum});
+      final state = OutlinedButtonThemeState.withTheme(elevation: prop);
 
       await pumpApp(tester, state);
 
-      await widgetTesters.checkTextField(
-        tester,
-        'outlinedButtonThemeEditor_elevationTextField_default',
-        doubleValue,
+      await tester.expectTextField(key, doubleStr);
+    });
+
+    testWidgets('change value', (tester) async {
+      await pumpApp(tester);
+      await tester.verifyTextField(
+        key,
+        doubleStr,
+        outlinedButtonThemeCubit.elevationChanged,
       );
-    },
-  );
+    });
+  });
 }

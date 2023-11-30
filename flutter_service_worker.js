bda690d6b3de8886bb73311deb53da2084e8cdb4
zeshuaro <joshuaystang@gmail.com>
2023-11-30 12:03:08 +0000

    Deploying to gh-pages from @ zeshuaro/appainter@15b5f7dd683693077067aa1fe988d1df22f5b49e 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,10 +5,10 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "447fc2245bc32d7418ff21db53c64d75",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
-"version.json": "6137d01f301e2bbc85eb9d13317be272",
+"version.json": "2805d25477973e0c84e377ae0e86c59a",
 "favicon.ico": "02cc314f1888171f9ec195c1a97d1acb",
-"index.html": "265ad2685650092cab5abb1cc95aa910",
-"/": "265ad2685650092cab5abb1cc95aa910",
+"index.html": "35a219bcdc2461013ef96b687ecd6042",
+"/": "35a219bcdc2461013ef96b687ecd6042",
 "icons/android-chrome-512x512.png": "f397aae3a660e3af9dfee292ee3e2bf9",
 "icons/android-chrome-192x192.png": "e03f23fd41872367b2ba387b50f87257",
 "icons/apple-touch-icon.png": "b1e4bbb6ad1d9d7f3ae57d46af16d2a9",

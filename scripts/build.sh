30768da20ee2cfaded8835e89e1718291dfe4cfd
Joshua Tang <joshuaystang@gmail.com>
2023-12-03 18:02:18 +1100

    ci: fix build script syntax (#965)

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,10 @@
 if [[ -z "${APP_TYPE}" ]]; then
     echo "APP_TYPE undefined"
     exit 1
-elif [[ -z "${BUILD_PATH}" ]] then
+elif [[ -z "${BUILD_PATH}" ]]; then
     echo "BUILD_PATH undefined"
     exit 1
-elif [[ -z "${BUILD_OUTPUT}" ]] then
+elif [[ -z "${BUILD_OUTPUT}" ]]; then
     echo "BUILD_OUTPUT undefined"
     exit 1
 fi
@@ -14,7 +14,7 @@ fi
 flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
 
-if [ "${APP_TYPE}" == "web" ] then
+if [ "${APP_TYPE}" == "web" ]; then
     flutter build web --source-maps --no-tree-shake-icons --dart-define=FLUTTER_WEB_USE_SKIA=true
     flutter packages pub run sentry_dart_plugin
 else

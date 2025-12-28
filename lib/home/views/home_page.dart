6f1c141515f8496137cea7d80f7abc86c07c6509
Joshua Tang <joshuaystang@gmail.com>
2025-12-29 01:13:47 +1100

    feat(sdk): update dependency flutter to v3.38.0 (#1755)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  static const _sdkVersion = '3.35.0+';
+  static const _sdkVersion = '3.38.0+';
   static final _backgroundColorDark = Colors.grey[900]!;
   static final _backgroundColorLight = Colors.grey[200]!;
 
